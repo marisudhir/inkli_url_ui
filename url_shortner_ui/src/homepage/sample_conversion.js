9d5c16c87ee241e5fab3f0c21de18112b18b27b6
marisudhir <marisudhirinklidox@gmail.com>
2025-04-13 00:46:01 +0530

    sample_ip updated

--- url_shortner_ui/src/homepage/sample_conversion.js
+++ url_shortner_ui/src/homepage/sample_conversion.js
@@ -87,8 +87,8 @@ function ShortURLSample() {
     const [copySnackbarOpen, setCopySnackbarOpen] = useState(false);
 
     const shortenedUrlRef = useRef(null);
-    const API_BASE_URL = "http://http://localhost:3000"; // Define base URL
-
+    // const API_BASE_URL = "http://localhost:3000"; // Define base URL
+    const API_BASE_URL = "http://143.110.246.124"; // Define base URL
     // --- Load anonymous count from local storage on mount ---
     useEffect(() => {
         const storedCount = localStorage.getItem('anonymousUrlCount');
